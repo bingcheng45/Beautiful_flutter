b07d9542332a28737de332a2a7ebda7bc096aa66
bingcheng45 <bingcheng45@gmail.com>
2019-10-07 22:56:48 +0800

    coded chat message

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,98 @@
 //test
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Friendlychat',
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+       child: new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  new InputDecoration.collapsed(hintText: "send a message"),
+            ),
+          ),
+          new Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: new Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Friendlychat'),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget{
+    ChatMessage({this.text});
+    final String text;
+    
+    @override
+    Widget build(BuildContext context) {
+      const String _name = "Bing Cheng";
+      return new Container(
+        margin: EdgeInsets.symmetric(vertical: 8.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0]),),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name, style: Theme.of(context).textTheme.subhead),
+                new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text),
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
+  }
