3cbc63f3f68fc56ed0ffe09e583e73604baa79f1
bingcheng45 <bingcheng45@gmail.com>
2019-10-13 12:23:59 +0800

    animation done

--- lib/main.dart
+++ lib/main.dart
@@ -20,38 +20,16 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
-  Widget _buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-       child: new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration:
-                  new InputDecoration.collapsed(hintText: "send a message"),
-            ),
-          ),
-          new Container(
-            margin: EdgeInsets.symmetric(horizontal: 4.0),
-            child: IconButton(
-              icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text),
-            ),
-          ),
-        ],
-      ),
-    ),
-    );
-  }
 
-  void _handleSubmitted(String text) {
-    _textController.clear();
+  @override
+  void dispose(){
+    for ( ChatMessage message in _messages){
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 
   @override
@@ -60,26 +38,91 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
         title: new Text('Friendlychat'),
       ),
-      body: _buildTextComposer(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "send a message"),
+              ),
+            ),
+            new Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+          duration: new Duration(milliseconds: 700), vsync: this),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
   }
 }
 
-class ChatMessage extends StatelessWidget{
-    ChatMessage({this.text});
-    final String text;
-    
-    @override
-    Widget build(BuildContext context) {
-      const String _name = "Bing Cheng";
-      return new Container(
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    const String _name = "Bing Cheng";
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      child: new Container(
         margin: EdgeInsets.symmetric(vertical: 8.0),
         child: new Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             new Container(
               margin: const EdgeInsets.only(right: 16.0),
-              child: new CircleAvatar(child: new Text(_name[0]),),
+              child: new CircleAvatar(
+                child: new Text(_name[0]),
+              ),
             ),
             new Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -93,6 +136,7 @@ class ChatMessage extends StatelessWidget{
             )
           ],
         ),
-      );
-    }
+      ),
+    );
   }
+}
